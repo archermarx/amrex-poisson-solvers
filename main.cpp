078a98092ffbda799565254ecf143f1e9fbffdf4
Thomas Marks <marksta@umich.edu>
2024-08-26 15:58:49 -0400

    Rename helloworld

--- main.cpp
+++ main.cpp
@@ -3,10 +3,8 @@
 
 int main (int argc, char *argv[]) {
   amrex::Initialize(argc, argv);
-#if 0
   {
     amrex::Print() << "Hello world from AMReX version " << amrex::Version() << "\n";
   }
-#endif 
   amrex::Finalize();
 }
